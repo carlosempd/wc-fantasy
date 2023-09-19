41d573fa1f828e716d60643023b6ea7849474f27
Carlos <carlosempd@gmail.com>
2023-09-19 12:55:14 -0400

    ✨ feat: crated info component and displayed at home component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { ToolbarModule } from 'src/app/shared/components/toolbar/toolbar.module';
 import { HomeRoutingModule } from './home-routing.module';
+import { InfoComponent } from './components/info/info.component';
+
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    InfoComponent
   ],
   imports: [
     CommonModule,
     ToolbarModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatListModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
