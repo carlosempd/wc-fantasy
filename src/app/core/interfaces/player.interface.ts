c2328f737dbb5391be1f8a63b55ea8a151e2c66f
Carlos <carlosempd@gmail.com>
2023-09-20 09:39:50 -0400

    💄 ui: updated global styles, added utils interface

--- src/app/core/interfaces/player.interface.ts
+++ src/app/core/interfaces/player.interface.ts
@@ -1,22 +1,22 @@
 export interface Player {
-    id: number
-    name: string
-    firstname: string
-    lastname: string
-    age: number
-    birth: Birth
-    nationality: string
-    height: string
-    weight: string
-    injured: boolean
-    photo: string,
+    id: number;
+    name: string;
+    firstname: string;
+    lastname: string;
+    age: number;
+    birth: Birth;
+    nationality: string;
+    height: string;
+    weight: string;
+    injured: boolean;
+    photo: string;
     position: POSITION;
 }
   
 export interface Birth {
-    date: string
-    place: string
-    country: string
+    date: string;
+    place: string;
+    country: string;
 }
 
 export enum POSITION {
