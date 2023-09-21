fd33ee214b7ade7a7c30d4158986993a2a5e5e18
Carlos <carlosempd@gmail.com>
2023-09-20 22:36:38 -0400

    ✨ feat: added arrange method at my-team component

--- src/app/pages/draft/components/my-team/my-team.component.ts
+++ src/app/pages/draft/components/my-team/my-team.component.ts
@@ -1,21 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Player } from 'src/app/core/interfaces/player.interface';
+import { Subscription } from 'rxjs';
+import { POSITION, Player } from 'src/app/core/interfaces/player.interface';
 import { MockService } from 'src/app/core/services/mock.service';
+import { PlayerService } from 'src/app/core/services/player.service';
 
 @Component({
   selector: 'app-my-team',
   templateUrl: './my-team.component.html',
   styleUrls: ['./my-team.component.scss']
 })
-export class MyTeamComponent {
+export class MyTeamComponent implements OnInit {
   myPlayers: Player[] = [];
+  defenders: Player[] = [];
+  midfielders: Player[] = [];
+  strikers: Player[] = [];
+  goalkeepers: Player[] = [];
+  subscription: Subscription = new Subscription();
 
   constructor(
     private mockService: MockService,
-    private router: Router
-  ) {
-    this.myPlayers = mockService.getPlayers().slice(0,17);
+    private router: Router,
+    private playerService: PlayerService,
+  ) {}
+  ngOnInit(): void {    
+    this.subscription.add(
+      this.playerService.draftedPlayersSubject.subscribe(list => {
+        this.myPlayers = list;
+        this.arrangePlayers();
+      })
+    );
+    this.arrangePlayers()
+  }
+
+  arrangePlayers() {
+    this.myPlayers.forEach(element => {
+      switch (element.position) {
+        case POSITION.def:
+          this.defenders.push(element);
+          break;
+        case POSITION.mid:
+          this.midfielders.push(element);
+          break;
+        case POSITION.str:
+          this.strikers.push(element);
+          break;
+        case POSITION.gk:
+          this.goalkeepers.push(element);
+          break;   
+        default:
+          break;
+      }
+    })
   }
 
   goToDraft() {
